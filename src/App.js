4233cf96e7f02a0458070254e191113426c89f69
jazz-code <ren.meta@gmail.com>
2019-08-12 19:55:32 -0400

    finished removeItem function

--- src/App.js
+++ src/App.js
@@ -18,25 +18,23 @@ function App() {
     setCart([...cart, item]);
   };
 
-  //   const removeItem = id => {
-  //     console.log("remove" + id);
-  //     // const newItems = [...cart.filter(items => items !== item.id)];
-
-  //     // setCart([...cart, newItems]);
-  //   };
+  const removeItem = id => {
+    console.log("remove" + id);
+    setCart(cart.filter(item => item.id !== id));
+  };
 
   return (
-    <div className="App">
-      <ProductContext.Provider value={{ products, addItem }}>
-        <CartContext.Provider value={{ cart }}>
+    <ProductContext.Provider value={{ products, addItem }}>
+      <CartContext.Provider value={{ cart, removeItem }}>
+        <div className="App">
           <Navigation cart={cart} />
           {/* Routes */}
           <Route exact path="/" component={Products} />
 
           <Route path="/cart" component={ShoppingCart} />
-        </CartContext.Provider>
-      </ProductContext.Provider>
-    </div>
+        </div>
+      </CartContext.Provider>
+    </ProductContext.Provider>
   );
 }
 
--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,12 +1,6 @@
 import React from "react";
 
 const Item = props => {
-  const removeItem = id => {
-    console.log("remove" + id);
-    const newItems = [...props.cart.filter(item => item !== id)];
-
-    props.setCart([...props.cart, newItems]);
-  };
   return (
     <div className="shopping-cart_item">
       <img src={props.image} alt={`${props.title} book`} />
@@ -14,7 +8,9 @@ const Item = props => {
       <div>
         <h1>{props.title}</h1>
         <p>$ {props.price}</p>
-        <button onClick={() => removeItem(props.id)}>Remove from cart</button>
+        <button onClick={() => props.removeItem(props.id)}>
+          Remove from cart
+        </button>
       </div>
     </div>
   );
