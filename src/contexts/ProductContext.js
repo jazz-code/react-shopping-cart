048345e2a79350f437e22e20311cd87b9a857f08
jazz-code <ren.meta@gmail.com>
2019-08-12 16:17:23 -0400

    fixed ProductContext

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,5 +1,5 @@
-import React, { createContext } from "react";
+import { createContext } from "react";
 
-const ProductContext = () => {};
+const ProductContext = createContext();
 
 export default ProductContext;
