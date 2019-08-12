0f54a6e6a65c5bb3b9e4d76a3bfd213ca2ab2942
jazz-code <ren.meta@gmail.com>
2019-08-12 16:36:06 -0400

    fixed error from adding useContext in Product instead Products component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,36 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
-
-import ProductContext from "./contexts/ProductContext"
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
+// Contexts
+import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		setCart([...cart, item]);
-	};
+  const addItem = item => {
+    setCart([...cart, item]);
+  };
 
-	return (
-		<ProductContext.Provider value={{products, addItem}}>
-		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				component={Products}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-		</ProductContext.Provider>
-	);
+  return (
+    <ProductContext.Provider value={{ products, addItem }}>
+      <CartContext.Provider value={cart}>
+        <div className="App">
+          <Navigation cart={cart} />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
+          )} />
+          <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+        </div>
+      </CartContext.Provider>
+    </ProductContext.Provider>
+  );
 }
 
 export default App;
