090bfbadfbc4e45c22bd795881b584ee613e10c7
jazz-code <ren.meta@gmail.com>
2019-08-12 16:38:55 -0400

    providing data with CartContext

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
           {/* Routes */}
           <Route exact path="/" component={Products} />
           )} />
-          <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+          <Route path="/cart" component={ShoppingCart} />} />
         </div>
       </CartContext.Provider>
     </ProductContext.Provider>
